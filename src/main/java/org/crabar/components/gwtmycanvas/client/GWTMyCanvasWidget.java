1b39ebf005901957f89b7dd858e483fa6307115f
Yaroslav Poliakov (Crabar) <ypoliakov@playtika.com.ua>
2014-12-26 17:15:16 +0200

    new intersection

--- src/main/java/org/crabar/components/gwtmycanvas/client/GWTMyCanvasWidget.java
+++ src/main/java/org/crabar/components/gwtmycanvas/client/GWTMyCanvasWidget.java
@@ -3,17 +3,21 @@ package org.crabar.components.gwtmycanvas.client;
 import com.google.gwt.animation.client.AnimationScheduler;
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.*;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
-import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.*;
 import org.crabar.components.gwtmycanvas.client.gameObjects.Ball;
+import org.crabar.components.gwtmycanvas.client.gameObjects.Brick;
 import org.crabar.components.gwtmycanvas.client.gameObjects.IDynamicObject;
 import org.crabar.components.gwtmycanvas.client.gameObjects.Platform;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Crabar on 08.11.2014.
@@ -23,26 +27,57 @@ public class GWTMyCanvasWidget extends Composite {
     public static final String CLASSNAME = "mycomponent";
     private static final int FRAME_RATE = 30;
     private Canvas canvas;
-    private Canvas backBufferCanvas;
+    private Canvas levelCanvas;
+    private AbsolutePanel panel;
     private Platform platform;
     private Ball ball;
     private ArrayList<IDynamicObject> gameObjects;
+    private ArrayList<Brick> bricks;
     private boolean needDraw = true;
 
     public GWTMyCanvasWidget() {
+        panel = new AbsolutePanel();
         canvas = Canvas.createIfSupported();
-        backBufferCanvas = Canvas.createIfSupported();
-        initHandlers();
-        initWidget(canvas);
-        initGameTimer();
+        levelCanvas = Canvas.createIfSupported();
+        initWidget(panel);
+        panel.add(levelCanvas, 0, 0);
+        panel.add(canvas, 0, 0);
+        levelCanvas.getElement().getStyle().setZIndex(0);
+        canvas.getElement().getStyle().setZIndex(1);
+        resizePanel(Window.getClientWidth(), Window.getClientHeight());
         resizeCanvas(canvas, Window.getClientWidth(), Window.getClientHeight());
-        resizeCanvas(backBufferCanvas, Window.getClientWidth(), Window.getClientHeight());
+        resizeCanvas(levelCanvas, Window.getClientWidth(), Window.getClientHeight());
+        initHandlers();
+        initGameTimer();
         setStyleName(CLASSNAME);
         gameObjects = new ArrayList<IDynamicObject>();
         platform = createPlatform();
         ball = createBall();
         gameObjects.add(platform);
         gameObjects.add(ball);
+        generateLevel();
+    }
+
+    public Logger getLogger() {
+        return Logger.getLogger("GWTMyCanvasWidget.java");
+    }
+
+    private void resizePanel(int width, int height) {
+        panel.setWidth(width + "px");
+        panel.setHeight(height + "px");
+    }
+
+    private void generateLevel() {
+        SimpleLevelGenerator levelGenerator = new SimpleLevelGenerator();
+        bricks = levelGenerator.generateLevel(100, 100, Window.getClientWidth() - 200, Window.getClientHeight() - 400);
+        getLogger().log(Level.SEVERE, String.valueOf(bricks.size()));
+        gameObjects.addAll(bricks);
+        getLogger().log(Level.SEVERE, String.valueOf(gameObjects.size()));
+        levelCanvas.getContext2d().clearRect(0, 0, canvas.getCoordinateSpaceWidth(), canvas.getCoordinateSpaceHeight());
+
+        for (Brick brick : bricks) {
+            brick.draw(levelCanvas.getContext2d());
+        }
     }
 
     private void initHandlers() {
@@ -57,8 +92,9 @@ public class GWTMyCanvasWidget extends Composite {
         Window.addResizeHandler(new ResizeHandler() {
             @Override
             public void onResize(ResizeEvent resizeEvent) {
-                resizeCanvas(canvas, resizeEvent.getWidth(), resizeEvent.getHeight());
-                resizeCanvas(backBufferCanvas, resizeEvent.getWidth(), resizeEvent.getHeight());
+                resizePanel(Window.getClientWidth(), Window.getClientHeight());
+                resizeCanvas(canvas, Window.getClientWidth(), Window.getClientHeight());
+                resizeCanvas(levelCanvas, Window.getClientWidth(), Window.getClientHeight());
             }
         });
         canvas.addKeyDownHandler(new KeyDownHandler() {
@@ -78,7 +114,7 @@ public class GWTMyCanvasWidget extends Composite {
     private Ball createBall() {
         Ball ball = new Ball();
         ball.setCenterX(Window.getClientWidth() / 2);
-        ball.setCenterY(Window.getClientHeight() / 2);
+        ball.setCenterY(Window.getClientHeight() - 100);
         return ball;
     }
 
@@ -106,9 +142,6 @@ public class GWTMyCanvasWidget extends Composite {
         });
     }
 
-    private void drawBuffer(Context2d bufferContext) {
-        canvas.getContext2d().drawImage(bufferContext.getCanvas(), 0, 0);
-    }
 
     private void drawFPS(Context2d canvas, double elapsedTime) {
         canvas.setFont("20pt serif");
@@ -116,17 +149,29 @@ public class GWTMyCanvasWidget extends Composite {
     }
 
     private void drawCanvas(double elapsedTime) {
-//        backBufferCanvas.getContext2d().clearRect(0, 0, backBufferCanvas.getCoordinateSpaceWidth(), backBufferCanvas.getCoordinateSpaceHeight());
+//        levelCanvas.getContext2d().clearRect(0, 0, levelCanvas.getCoordinateSpaceWidth(), levelCanvas.getCoordinateSpaceHeight());
         canvas.getContext2d().clearRect(0, 0, canvas.getCoordinateSpaceWidth(), canvas.getCoordinateSpaceHeight());
         for (IDynamicObject gameObject : gameObjects) {
-            gameObject.update(elapsedTime);
+            if (!(gameObject instanceof Brick)) {
+                gameObject.draw(canvas.getContext2d());
+            }
         }
         for (IDynamicObject gameObject : gameObjects) {
-            gameObject.draw(canvas.getContext2d());
+            if (!(gameObject instanceof Brick)) {
+                gameObject.update(elapsedTime);
+            }
+
         }
         ball.collideWithWalls(0, 0, canvas.getCoordinateSpaceWidth(), canvas.getCoordinateSpaceHeight());
         ball.collideWithObject(platform, false);
-//        drawBuffer(canvas.getContext2d());
+
+        Boolean intersection;
+        for (Brick brick : bricks) {
+//            intersection = ball.collideWithObject2(brick, false);
+//            if (intersection) {
+//                break;
+//            }
+        }
     }
 
     private Platform createPlatform() {
--- src/main/java/org/crabar/components/gwtmycanvas/client/gameObjects/Ball.java
+++ src/main/java/org/crabar/components/gwtmycanvas/client/gameObjects/Ball.java
@@ -2,11 +2,11 @@ package org.crabar.components.gwtmycanvas.client.gameObjects;
 
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.dom.client.ImageElement;
+import com.google.gwt.touch.client.*;
 import com.google.gwt.user.client.ui.Image;
 import org.crabar.components.gwtmycanvas.client.resources.CraberoidClientBundle;
 
-import java.awt.*;
-import java.util.logging.Level;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 /**
@@ -17,6 +17,9 @@ public class Ball implements IDynamicObject {
     ImageElement ballImage;
     int centerX;
     int centerY;
+    int previousCenterX;
+    int previousCenterY;
+
     int radius = 20;
     double velocity[] = {0, 0}; // px per sec
 
@@ -50,11 +53,19 @@ public class Ball implements IDynamicObject {
     }
 
     public void collideWithWalls(int left, int top, int right, int bottom) {
-        if ((getCenterX() - getRadius() < left) || (getCenterX() + getRadius() > right)) {
+        if (getCenterX() - getRadius() < left) {
+            velocity[0] = - velocity[0];
+        }
+        if (getCenterX() + getRadius() > right) {
+            double yIntersect = Math.sqrt(Math.pow(getRadius(), 2) - Math.pow(getCenterX() - right, 2)) + getCenterY();
             velocity[0] = - velocity[0];
         } else if (getCenterY() - getRadius() < top || (getCenterY() + getRadius() > bottom)) {
             velocity[1] = - velocity[1];
         }
+    }
+
+    private double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x1 - x2 , 2) + Math.pow(y1 - y2, 2));
     }
 
     public void collideWithObject(Rectangle object, Boolean smartPhysic) {
@@ -87,6 +98,65 @@ public class Ball implements IDynamicObject {
         }
     }
 
+    public Boolean collideWithObject2(Rectangle object, Boolean smartPhysic) {
+        ArrayList<SideVO> sides = new ArrayList<SideVO>();
+        sides.add(new SideVO(object.getX(), object.getY(), object.getX(), object.getY() + object.getHeight(), "left"));
+        sides.add(new SideVO(object.getX() + object.getWidth(), object.getY(), object.getX() + object.getWidth(), object.getY() + object.getHeight(), "right"));
+        sides.add(new SideVO(object.getX(), object.getY(), object.getX() + object.getWidth(), object.getY(), "top"));
+        sides.add(new SideVO(object.getX(), object.getY() + object.getY(), object.getX() + object.getWidth(), object.getY() + object.getHeight(), "bottom"));
+
+        Boolean isIntersect = false;
+
+        for (SideVO side : sides) {
+            isIntersect = isIntersect || isBallIntersectSide(side.x1, side.y1, side.x2, side.y2);
+        }
+
+        if (!isIntersect) {
+            return false;
+        }
+
+        SideVO wall = findIntersectionWall(sides);
+
+        switch (wall.name) {
+            case "top":
+            case "bottom": {
+                velocity[1] = -velocity[1];
+            }
+            case "left":
+            case "right": {
+                velocity[0] = -velocity[0];
+            }
+        }
+
+        return true;
+
+//        Boolean intersectLeftSide = isBallIntersectSide(object.getX(), object.getY(), object.getX(), object.getY() + object.getHeight());
+//        Boolean intersectRightSide = isBallIntersectSide(object.getX() + object.getWidth(), object.getY(), object.getX() + object.getWidth(), object.getY() + object.getHeight());
+//        Boolean intersectTopSide = isBallIntersectSide(object.getX(), object.getY(), object.getX() + object.getWidth(), object.getY());
+//        Boolean intersectBottomSide = isBallIntersectSide(object.getX(), object.getY() + object.getY(), object.getX() + object.getWidth(), object.getY() + object.getHeight());
+
+    }
+
+    private SideVO findIntersectionWall(ArrayList<SideVO> sides) {
+        int sideIntersectionCounter = 1;
+        SideVO wall = sides.get(0);
+
+        while (sideIntersectionCounter > 0) {
+            sideIntersectionCounter = 0;
+            for (SideVO side : sides) {
+                if (isBallIntersectSide(side.x1, side.y1, side.x2, side.y2)) {
+                    sideIntersectionCounter++;
+                    wall = side;
+                }
+            }
+
+            centerX -= velocity[0] / 200;
+            centerY -= velocity[1] / 200;
+        }
+
+        return wall;
+    }
+
     private Boolean isBallIntersectSide(int x1, int y1, int x2, int y2) {
         int oX1 = x1 - (int) getCenterX();
         int oX2 = x2 - (int) getCenterX();
@@ -110,6 +180,10 @@ public class Ball implements IDynamicObject {
         return (a + b + c < 0);
     }
 
+//    private Point calcIntersectionOffset() {
+//
+//    }
+
 
     @Override
     public void draw(Context2d context2d) {
@@ -122,9 +196,11 @@ public class Ball implements IDynamicObject {
     @Override
     public void update(double elapsedTime) {
         double velocityFactor = 1000 / elapsedTime;
+        previousCenterX = centerX;
+        previousCenterY = centerY;
         centerX += Math.round(velocity[0] / velocityFactor);
         centerY += Math.round(velocity[1] / velocityFactor);
-        getLogger().log(Level.SEVERE, "x: " + String.valueOf(centerX) + " y: " + String.valueOf(centerY) + " elapsed time:" + String.valueOf(elapsedTime));
+//        getLogger().log(Level.SEVERE, "x: " + String.valueOf(centerX) + " y: " + String.valueOf(centerY) + " elapsed time:" + String.valueOf(elapsedTime));
     }
 
     public void setVelocity(int horizontalVelocity, int verticalVelocity) {
