7fbbc51e85734f1911f538ff9664da5fb21e3603
Yaroslav Poliakov (Crabar) <ypoliakov@playtika.com.ua>
2014-12-26 17:15:36 +0200

    new intersection (fix)

---
+++ src/main/java/org/crabar/components/gwtmycanvas/client/gameObjects/SideVO.java
@@ -0,0 +1,20 @@
+package org.crabar.components.gwtmycanvas.client.gameObjects;
+
+/**
+ * Created by ypoliakov on 26.12.2014.
+ */
+public class SideVO {
+    public int x1;
+    public int y1;
+    public int x2;
+    public int y2;
+    public String name;
+
+    public SideVO(int x1, int y1, int x2, int y2, String name) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.name = name;
+    }
+}
